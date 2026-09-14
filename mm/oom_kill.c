cea62aa2dd160db55aa5e06de8a0c378ede53e78
Leecaarmy <leecaarmy@users.noreply.github.com>
2026-09-15 00:02:12 +0800

    Resolve 5.4.302 stable merge conflict markers

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -42,12 +42,9 @@
 #include <linux/kthread.h>
 #include <linux/init.h>
 #include <linux/mmu_notifier.h>
-<<<<<<< ours
 #include <linux/show_mem_notifier.h>
 #include <linux/memory_hotplug.h>
-=======
 #include <linux/cred.h>
->>>>>>> theirs
 
 #include <asm/tlb.h>
 #include "internal.h"
@@ -747,12 +744,8 @@ static void __mark_oom_victim(struct task_struct *tsk)
  */
 static void mark_oom_victim(struct task_struct *tsk)
 {
-<<<<<<< ours
-=======
 	const struct cred *cred;
 	struct mm_struct *mm = tsk->mm;
-
->>>>>>> theirs
 	WARN_ON(oom_killer_disabled);
 	/* OOM killer might race with memcg OOM */
 	if (test_and_set_tsk_thread_flag(tsk, TIF_MEMDIE))
